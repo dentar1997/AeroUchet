d8fbce454c256b311cd0eb98b44c563d3c54f372
dentar1997 <dentar1997@gmail.com>
2026-09-24 02:06:10 +0300

    Test GitHub to Playgrounds sync

--- SettingsView.swift
+++ SettingsView.swift
@@ -401,7 +401,7 @@ struct SettingsRootView: View {
                         HStack {
                             
                             Label(
-                                "Расчётное время",
+                                "Расчётное время • GitHub",
                                 systemImage:
                                     "tablecells"
                             )
