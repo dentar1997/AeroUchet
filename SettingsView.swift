e42f6c884be97bc6a95008c20da6d4cbe525a617
dentar1997 <dentar1997@gmail.com>
2026-09-24 03:38:46 +0300

    Clean up shortcut test label

--- SettingsView.swift
+++ SettingsView.swift
@@ -401,7 +401,7 @@ struct SettingsRootView: View {
                         HStack {
                             
                             Label(
-                                "Расчётное время • AUTO",
+                                "Расчётное время",
                                 systemImage:
                                     "tablecells"
                             )
