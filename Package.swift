10c4757f53a0cd5217005d334399ae9869a08e92
dentar1997 <dentar1997@gmail.com>
2026-09-24 03:38:48 +0300

    Revert launch URL scheme experiment

--- Package.swift
+++ Package.swift
@@ -29,17 +29,13 @@ let package = Package(
                 .landscapeRight,
                 .landscapeLeft,
                 .portraitUpsideDown(.when(deviceFamilies: [.pad]))
-            ],
-            additionalInfoPlistContentFilePath: "AeroUchetInfo.plist"
+            ]
         )
     ],
     targets: [
         .executableTarget(
             name: "AppModule",
             path: ".",
-            exclude: [
-                "AeroUchetInfo.plist"
-            ],
             swiftSettings: [
                 .enableUpcomingFeature("BareSlashRegexLiterals")
             ]
